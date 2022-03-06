f78e7330051643c07560802117b23a23e678c993
Driijko <driijko@gmail.com>
2022-03-06 18:10:29 -0500

    Splash draft 8.

--- splash/splash.js
+++ splash/splash.js
@@ -1,5 +1,5 @@
 gsap.set("#album-cover", {
-  opacity: 0,
+  opacity: 1,
 })
 
 gsap.to("#album-cover", {
@@ -12,5 +12,25 @@ gsap.to("#filter-effect1", {
     stdDeviation: 0,
   },
   ease: "ease-in",
-}); 
+});
 
+document.getElementById("enter-link").addEventListener("click", e => {
+  e.preventDefault();
+  gsap.to("#album-cover", {
+    duration: 5,
+    opacity: 0,
+  });
+  gsap.to("#filter-effect1", {
+    duration: 5,
+    attr: {
+      stdDeviation: 50,
+    },
+  });
+  gsap.to(e.target, {
+    duration: 5,
+    opacity: 0,
+  });
+  setTimeout(()=> {
+    window.location.href = "./menu.html";
+  }, 6000)
+})
